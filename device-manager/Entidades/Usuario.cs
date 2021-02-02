ee736983ee6d2886567d8e56f14cf22608a9c9ac
Jesse Miranda <nandopox@gmail.com>
2021-02-02 17:29:55 -0300

    Criada a classe Usuario e seus métodos

---
+++ device-manager/Entidades/Usuario.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entidades
+{
+    class Usuario
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Email { get; set; }
+
+        public Usuario()
+        {
+        }
+
+        public Usuario(string nome, string cPF, string email)
+        {
+            Nome = nome;
+            CPF = cPF;
+            Email = email;
+        }
+
+        public static void criarUsuario(List<Usuario> usuario)
+        {
+            Console.Write("Nome: ");
+            var nome = Console.ReadLine();
+            Console.Write("CPF: ");
+            var cpf = Console.ReadLine();
+            Console.Write("Email: ");
+            var email = Console.ReadLine();
+            usuario.Add(new Usuario(nome, cpf, email));
+        }
+
+        public static void editarUsuario(List<Usuario> usuario)
+        {
+            Console.Write("CPF a editar: ");
+            var cpf = Console.ReadLine();
+            var editar = usuario.Find(x => x.CPF == cpf);
+            Console.Write("Nome: ");
+            editar.Nome = Console.ReadLine();
+            Console.Write("CPF: ");
+            editar.CPF = Console.ReadLine();
+            Console.Write("Email: ");
+            editar.Email = Console.ReadLine();
+        }
+
+        public static void excluirUsuario(List<Usuario> usuario)
+        {
+            Console.Write("CPF a excluir: ");
+            var cpf = Console.ReadLine();
+            usuario.Remove(usuario.Find(x => x.CPF == cpf));
+        }
+
+        public static void ConsultarUsuario(List<Usuario> usuario)
+        {
+            Console.Write("CPF: ");
+            var cpf = Console.ReadLine();
+            Console.WriteLine(usuario.Find(x => x.CPF == cpf));
+        }
+
+        public override string ToString()
+        {
+            return "Nome: "
+                + Nome
+                + "\n"
+                +"CPF: "
+                + CPF
+                + "\n"
+                + "E-mail: "
+                + Email;
+        }
+    }
+}
--- device-manager/Program.cs
+++ device-manager/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
 
 namespace device_manager
 {
@@ -6,7 +8,37 @@ namespace device_manager
     {
         static void Main(string[] args)
         {
-            
+            List<Usuario> usuario = new List<Usuario>();
+            Usuario.criarUsuario(usuario);
+            Usuario.criarUsuario(usuario);
+
+            Console.Clear();
+            Console.WriteLine("===USUARIOS CADASTRADOS===");
+            foreach (var item in usuario)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine();
+            }
+
+            Usuario.editarUsuario(usuario);
+
+            Console.WriteLine();
+            Console.WriteLine("===USUARIOS ATUALIZADOS===");
+            foreach (var item in usuario)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine();
+            }
+
+            Usuario.excluirUsuario(usuario);
+
+            Console.WriteLine();
+            Console.WriteLine("===USUARIOS ATUALIZADOS===");
+            foreach (var item in usuario)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine();
+            }
         }
     }
 }
